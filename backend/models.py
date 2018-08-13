edec59b1bb87e2dcc65886f7a9eb1e9a22e374f1
anyi <759502117@qq.com>
2018-08-13 16:43:01 +0800

    age required

--- backend/models.py
+++ backend/models.py
@@ -14,7 +14,7 @@ from django.db import models
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.IntegerField(default=None,blank=True, null=True)
 
     def __str__(self):
             return self.name
